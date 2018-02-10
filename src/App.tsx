fcf82ca8b2e2668c83e495a321c977d4c840ab50
Miro Nurmela <miro.nurmela@futurice.com>
2018-02-11 00:12:41 +0200

    Some better explanation

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ class App extends React.Component<{}, AppState> {
     return (
       <div className="AppContainer">
         <h1>React Exercise</h1>
-        <h4>In idiomatic TypeScript, implement what is shown in the gif below</h4>
+        <h4>In idiomatic TypeScript and React, implement what is shown in the gif below</h4>
+        <h4>Exact styling is not top priority, you should aim for the functionality</h4>
+        <h5>(Screen recorder stuff of course excluded, the controls and the red click circles)</h5>
         <img src={gif} width={'90%'} />
       </div>
     );
