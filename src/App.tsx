d56ef511493ffcb07cb36279dd59af5a4dbbfa48
Miro Nurmela <miro.nurmela@futurice.com>
2018-02-11 00:09:55 +0200

    Add exampl gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './App.css';
 
+const gif = require('./example.gif');
+
 interface AppState {
 
 }
@@ -11,6 +13,7 @@ class App extends React.Component<{}, AppState> {
       <div className="AppContainer">
         <h1>React Exercise</h1>
         <h4>In idiomatic TypeScript, implement what is shown in the gif below</h4>
+        <img src={gif} width={'90%'} />
       </div>
     );
   }
